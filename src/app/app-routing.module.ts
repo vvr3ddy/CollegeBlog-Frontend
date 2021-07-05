dd13c5d5d873af39b4c464594a440e517622b3ce
vvr3ddy <vvr-rockstar@bmsit.in>
2021-07-05 18:54:06 +0530

    :octocat: Initial Structure
    Signed-off-by: vvr3ddy <vvr-rockstar@bmsit.in>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,50 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './shared/about/about.component';
+import { HomeComponent } from './shared/home/home.component';
+import { LoginComponent } from './shared/login/login.component';
+import { SignupComponent } from './shared/signup/signup.component';
+import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'signup',
+    component: SignupComponent
+  },
+  {
+    path: 'college',
+    loadChildren: () => import('./modules/college/college.module').then(m => m.CollegeModule)
+  },
+  {
+    path: 'faculty',
+    loadChildren: () => import('./modules/faculty/faculty.module').then(m => m.FacultyModule)
+  },
+  {
+    path: 'student',
+    loadChildren: () => import('./modules/student/student.module').then(m => m.StudentModule)
+  },
+  {
+    path: "**",
+    redirectTo: '/404'
+  },
+  {
+    path: '404',
+    component: PageNotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { NavigationComponent } from './shared/navigation/navigation.component';
+import { SignupComponent } from './shared/signup/signup.component';
+import { LoginComponent } from './shared/login/login.component';
+import { HomeComponent } from './shared/home/home.component';
+import { AboutComponent } from './shared/about/about.component';
+import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    NavigationComponent,
+    SignupComponent,
+    LoginComponent,
+    HomeComponent,
+    AboutComponent,
+    PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
